d2cc223e9a9dd064900c49fa3bf8b13ea0c836f7
Cleberson F R Junior <cleberson.dev@gmail.com>
2024-10-04 11:29:19 -0300

    Create Layout component

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,51 +1,23 @@
 <template>
-  <div
-    class="flex h-[100svh] dark:bg-[#212529] dark:text-white/90"
-    :class="{ dark: themeStore.mode === 'dark' }"
-  >
-    <my-header v-if="!isCollapsed" />
-
-    <main class="h-full overflow-y-auto flex-grow">
-      <router-view />
-    </main>
-
-    <div class="absolute bottom-1 right-1 font-mono">{{ appVersion }}</div>
-  </div>
-
+  <Layout>
+    <router-view />
+  </Layout>
   <Notification />
   <ModalContainer />
 </template>
 
 <script setup lang="tsx">
-import { onMounted, onUnmounted, ref } from "vue";
+import { onMounted } from "vue";
 import Notification from "./components/Notification.vue";
 import { useMainStore } from "./store/main.store";
 
-import MyHeader from "./components/MyHeader.vue";
 import ModalContainer from "./components/ModalContainer.vue";
 import ConfirmationDialog from "./components/ConfirmationDialog.vue";
-import { useThemeStore } from "./store/theme.store";
 import useModal from "./composables/useModal";
 import listenToMainEvents from "./utils/listeners";
-
-const appVersion = ref<string | undefined>();
+import Layout from "./components/Layout.vue";
 
 const mainStore = useMainStore();
-const themeStore = useThemeStore();
-
-const isCollapsed = ref(false);
-
-const addKeyShortcuts = (e: KeyboardEvent) => {
-  if (!e.ctrlKey) return;
-  if (e.key.toLowerCase() === "n") {
-    isCollapsed.value = !isCollapsed.value;
-  }
-
-  if (e.key.toLowerCase() === "b") {
-    themeStore.spacing = themeStore.spacing === "center" ? "normal" : "center";
-  }
-};
-
 const modal = useModal();
 
 function openConfirmationDialog(confirmFn: () => void) {
@@ -54,12 +26,6 @@ function openConfirmationDialog(confirmFn: () => void) {
 
 onMounted(() => {
   mainStore.initializeTime();
-  window.addEventListener("keyup", addKeyShortcuts);
   listenToMainEvents(openConfirmationDialog);
-  appVersion.value = window.electronAPI.appVersion;
-});
-
-onUnmounted(() => {
-  window.removeEventListener("keyup", addKeyShortcuts);
 });
 </script>
---
+++ src/renderer/components/Layout.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from "vue";
+import { useThemeStore } from "../store/theme.store";
+import Menu from "./Menu.vue";
+
+const themeStore = useThemeStore();
+const isCollapsed = ref(false);
+const appVersion = ref<string | undefined>();
+
+const addKeyShortcuts = (e: KeyboardEvent) => {
+  if (!e.ctrlKey) return;
+  if (e.key.toLowerCase() === "n") {
+    isCollapsed.value = !isCollapsed.value;
+  }
+
+  if (e.key.toLowerCase() === "b") {
+    themeStore.spacing = themeStore.spacing === "center" ? "normal" : "center";
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("keyup", addKeyShortcuts);
+  appVersion.value = window.electronAPI.appVersion;
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keyup", addKeyShortcuts);
+});
+</script>
+
+<template>
+  <div
+    class="flex h-[100svh] dark:bg-[#212529] dark:text-white/90"
+    :class="{ dark: themeStore.mode === 'dark' }"
+  >
+    <Menu v-if="!isCollapsed" />
+
+    <main class="h-full overflow-y-auto flex-grow">
+      <slot></slot>
+    </main>
+
+    <div class="absolute bottom-1 right-1 font-mono">{{ appVersion }}</div>
+  </div>
+</template>
